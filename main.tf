a7e72de1c505dc093e6c555a45e2789ba59d7687
SathyaNarayanan-tut <60778744+SathyaNarayanan-tut@users.noreply.github.com>
2025-07-16 15:09:03 +0300

    Updated main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,48 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
 provider "aws" {
-  access_key = "AKIA1234567890"       # Intentional secret
-  secret_key = "ABCDEF1234567890XYZ"  # Intentional secret
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
   region     = "us-east-1"
+}
+
+resource "aws_iam_role" "ec2_role" {
+  name = "ec2_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_instance_profile" "ec2_instance_profile" {
+  name = "ec2_instance_profile"
+  role = aws_iam_role.ec2_role.name
 }
 
 resource "aws_instance" "demo" {
   ami           = "ami-0c55b159cbfafe1f0"
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
+  monitoring    = true
+  ebs_optimized = true
+
+  metadata_options {
+    http_tokens   = "required"
+    http_endpoint = "enabled"
+  }
+
+  iam_instance_profile = aws_iam_instance_profile.ec2_instance_profile.name
 }
